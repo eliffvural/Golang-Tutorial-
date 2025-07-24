e824ac4dd31da4dbe2da396211bebdfceb303ab1
Elif Vural <elifvural019@gmail.com>
2025-07-24 15:07:15 +0300

    runes

--- strings.go
+++ strings.go
@@ -12,6 +12,11 @@ func main() {
 	sV1 := "a word"
 	replacer := strings.NewReplacer("a", "another")
 	sV2 := replacer.Replace(sV1)
-	pl(sV2) // a word another word
+	pl(sV2)                                                            // a word another word
+	pl("Length :", len(sV2))                                           // Length : 15
+	pl("Contains 'another':", strings.Contains(sV2, "another"))        // Contains 'another': true
+	pl("o index", strings.Index(sV2, "o"))                             // o index: 4
+	pl("Replace: ", strings.Replace(sV2, "another", "a different", 1)) // Replace: another word a different word
+	sV3 := "\nSome Words\n"
 
 }
