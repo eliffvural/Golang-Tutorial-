418548036e711f86731beeeaa88d7a2ccbd4b05b
Elif Vural <elifvural019@gmail.com>
2025-07-24 12:31:52 +0300

    casting string 2

--- castingstrings.go
+++ castingstrings.go
@@ -12,4 +12,8 @@ func main() {
 	cv3 := "50000000"
 	cv4, err := strconv.Atoi(cv3)
 	pl(cv4, err, reflect.TypeOf(cv4)) // 50000000 <nil> int
+
+	cv5 := 50000000
+	cv6 := strconv.Itoa(cv5)
+	pl(cv6)
 }
