43096bcc8fa18ab8088f6031e786c0ca4aa40105
Elif Vural <elifvural019@gmail.com>
2025-07-24 12:36:09 +0300

    casting strings 3 committed

--- castingstrings.go
+++ castingstrings.go
@@ -16,4 +16,11 @@ func main() {
 	cv5 := 50000000
 	cv6 := strconv.Itoa(cv5)
 	pl(cv6)
+
+	cv7 := "3.14"
+	if cv8, err := strconv.ParseFloat(cv7, 64); err != nil {
+		pl(cv8)
+	}
+	cv9 := fmt.Sprintf("%f", 3.14)
+	pl(cv9) // 3.140000
 }
